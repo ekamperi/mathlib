9939865490d8a8989f2d16e4b85953adb863dbf7
Stathis Kamperis <ekamperi@gmail.com>
2010-05-26 02:30:52 +0300

    subr_fpcmp: Add experimental fpcmp_equal()

--- subr_fpcmp.h
+++ subr_fpcmp.h
@@ -6,4 +6,25 @@
 #define	FPCMP_EQUAL(x, y)	\
 	(fabs((x) - (y)) < (1E-5 * fmax(fabs((x)), fabs((y)))))
 
-#endif	/* !__SUBR_FPCMP_H__ */
+#ifndef	iszero
+#define	iszero(x)	(fpclassify((x) == FP_ZERO))
+#endif
+
+int
+fpcmp_equal(double x, double y)
+{
+	if (isnan(x) || isnan(y))
+		return (isnan(x) && isnan(y));
+
+	if (isinf(x) || isinf(y))
+		return ((isinf(x) && isinf(y))
+		    && (signbit(x) == signbit(y)));
+
+	if (iszero(x) || iszero(y))
+		return ((iszero(x) && iszero(y))
+		    && (signbit(x) == signbit(y)));
+
+	return ((fabs(x) - y) < (1E-5 * fmax(fabs(x), fabs(y))));
+}
+
+#endif  /* !__SUBR_FPCMP_H__ */
