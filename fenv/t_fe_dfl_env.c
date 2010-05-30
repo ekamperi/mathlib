02441d752e4e25253b6d4b249a118f2f5203d2bf
stathis <stathis@opensolaris.(none)>
2010-05-30 22:43:39 +0300

    t_fe_dfl_env: Add more checks

--- fenv/t_fe_dfl_env.c
+++ fenv/t_fe_dfl_env.c
@@ -74,16 +74,16 @@ ATF_TC_BODY(test_feupdateenv, tc)
 }
 
 /*
- * Test case 3 -- memcpy()
+ * Test case 3 -- memcpy() + fesetenv()
  */
-ATF_TC(test_memcpy);
-ATF_TC_HEAD(test_memcpy, tc)
+ATF_TC(test_memcpy_fesetenv);
+ATF_TC_HEAD(test_memcpy_fesetenv, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "memcmpy()");
+	    "memcmpy() + fesetenv()");
 }
-ATF_TC_BODY(test_memcpy, tc)
+ATF_TC_BODY(test_memcpy_fesetenv, tc)
 {
 	fenv_t env1, env2;
 	fenv_t tmp;
@@ -113,11 +113,76 @@ ATF_TC_BODY(test_memcpy, tc)
 	ATF_CHECK(memcmp(&env1, &env2, sizeof(env1)) == 0);
 }
 
+/*
+ * Test case 4 -- memcpy() + feupdateenv()
+ */
+ATF_TC(test_memcpy_feupdateenv);
+ATF_TC_HEAD(test_memcpy_feupdateenv, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "memcmpy() + feupdateenv()");
+}
+ATF_TC_BODY(test_memcpy_feupdateenv, tc)
+{
+	fenv_t env1, env2;
+	fenv_t tmp;
+
+	/*
+	 * Since we are going to compare the two environments,
+	 * be paranoid and assign to them different contents.
+	 */
+	memset(&env1, 0x00, sizeof(env1));
+	memset(&env2, 0xFF, sizeof(env2));
+
+	/* Get the floating point environment installed at program startup */
+	ATF_REQUIRE(fegetenv(&env1) == 0);
+
+	/* Make a copy of it through FE_DFL_ENV */
+	memcpy(&tmp, FE_DFL_ENV, sizeof tmp);
+
+	/*
+	 * Set the floating point environment to the default one,
+	 * but do so via an intermediate variable.
+	 */
+	ATF_REQUIRE(feupdateenv(&tmp) == 0);
+
+	/* And get it again */
+	ATF_REQUIRE(fegetenv(&env2) == 0);
+
+	ATF_CHECK(memcmp(&env1, &env2, sizeof(env1)) == 0);
+}
+
+/*
+ * Test case 5 -- memcpy()
+ * XXX: Just curious, probably it will be removed later on.
+ */
+ATF_TC(test_memcpy);
+ATF_TC_HEAD(test_memcpy, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "memcmpy()");
+}
+ATF_TC_BODY(test_memcpy, tc)
+{
+	fenv_t env;
+
+	memset(&env, 0x00, sizeof(env));
+
+	/* Get the floating point environment installed at program startup */
+	ATF_REQUIRE(fegetenv(&env) == 0);
+
+	ATF_CHECK(memcmp(&env, FE_DFL_ENV, sizeof(env)));
+}
+
 /* Add test cases to test program */
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, test_fesetenv);
 	ATF_TP_ADD_TC(tp, test_feupdateenv);
+	ATF_TP_ADD_TC(tp, test_memcpy_fesetenv);
+	ATF_TP_ADD_TC(tp, test_memcpy_feupdateenv);
 	ATF_TP_ADD_TC(tp, test_memcpy);
 
 	return atf_no_error();
