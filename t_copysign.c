20a99bea503bf8dbd04db8bc810cf3a8bcbda51c
Stathis Kamperis <ekamperi@gmail.com>
2010-06-04 01:48:11 +0300

    t_copysign: Sigh, cleanup

--- t_copysign.c
+++ t_copysign.c
@@ -29,6 +29,10 @@ ATF_TC_BODY(test_copysign1, tc)
 {
 	size_t i, N;
 
+	N = sizeof(ttable) / sizeof(ttable[0]);
+
+	for (i = 0; i < N; i++) {
+	}
 }
 
 /*
@@ -39,35 +43,43 @@ ATF_TC_HEAD(test_copysign2, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "XXX");
+	    "Fuzz testing");
 }
 ATF_TC_BODY(test_copysign2, tc)
 {
-	double dx, dy, dz;
 	float fx, fy, fz;
+	double dx, dy, dz;
+	long double ldx, ldy, ldz;
 	long i, N;
 
 	N = get_config_var_as_long(tc, "iterations");
 	ATF_REQUIRE(N > 0);
 
-	for (i = 0; i < N; i++) {
+	ATF_FOR_LOOP(i, N, i++) {
 		dx = random_double(FP_NORMAL);
 		dy = random_double(FP_NORMAL);
 
 		fx = random_double(FP_NORMAL);
 		fy = random_double(FP_NORMAL);
 
+		ldx = random_double(FP_NORMAL);
+		ldy = random_double(FP_NORMAL);
+
 		/*
 		 * Upon successful completion, these functions shall return a
 		 * value with the magnitude of x and the sign of y.
 		 */
-		dz = copysign(dx, dy);
-		ATF_CHECK(fabs(dz) == fabs(dx));
-		ATF_CHECK(signbit(dz) == signbit(dy));
+		dz = copysignf(dx, dy);
+		ATF_PASS_OR_BREAK(fabsf(dz) == fabsf(dx));
+		ATF_PASS_OR_BREAK(signbit(dz) == signbit(dy));
 
-                fz = copysign(fx, fy);
-                ATF_CHECK(fabs(fz) == fabs(fx));
-                ATF_CHECK(signbit(fz) == signbit(fy));
+		fz = copysign(fx, fy);
+		ATF_PASS_OR_BREAK(fabs(fz) == fabs(fx));
+		ATF_PASS_OR_BREAK(signbit(fz) == signbit(fy));
+
+		ldz = copysignl(ldx, ldy);
+		ATF_PASS_OR_BREAK(fabsl(ldz) == fabsl(ldx));
+		ATF_PASS_OR_BREAK(signbit(ldz) == signbit(ldy));
 	}
 }
 
