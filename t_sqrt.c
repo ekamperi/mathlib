ecf3b533d61ece0a302a9c65e315c116822833e8
Stathis Kamperis <ekamperi@gmail.com>
2010-06-04 02:44:10 +0300

    t_sqrt: Add more checks, plus cleanup

--- t_sqrt.c
+++ t_sqrt.c
@@ -1,8 +1,9 @@
+#include <atf-c.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <atf-c.h>
+#include "subr_fpcmp.h"
 
 struct tentry {
 	double x;       /* Input */
@@ -26,15 +27,45 @@ ATF_TC_BODY(test_sqrt1, tc)
 
 	N = sizeof(ttable) / sizeof(ttable[0]);
 	for (i = 0; i < N; i++) {
+		/* Sanity check */
 		ATF_REQUIRE(ttable[i].x >= 0.0);
 
-		ATF_CHECK(fabs(sqrt(ttable[i].x) - ttable[i].y) < 1E-5);
+		/* Actual check */
+		ATF_CHECK(fpcmp_equal(sqrt(ttable[i].x), ttable[i].y));
 	}
 }
 
 /*
  * Test case 2 -- Edge cases
  */
+struct t2entry {
+	long double x;
+	long double y;
+} t2table[] = {
+	/* If x is NaN, a NaN shall be returned */
+#ifdef  NAN
+	{ NAN, NAN },
+#endif
+
+	/* If x is +-0, x shall be returned */
+	{  0.0,  0.0 },
+	{ -0.0, -0.0 },
+
+        /* If x is +Inf, x shall be returned */
+#ifdef  INFINITY
+	{ INFINITY, INFINITY },
+#endif
+#ifdef	HUGE_VAL
+	{ HUGE_VAL, HUGE_VAL },
+#endif
+#ifdef	HUGE_VALF
+	{ HUGE_VALF, HUGE_VALF },
+#endif
+#ifdef	HUGE_VALL
+	{ HUGE_VALL, HUGE_VALL },
+#endif
+};
+
 ATF_TC(test_sqrt2);
 ATF_TC_HEAD(test_sqrt2, tc)
 {
@@ -44,28 +75,26 @@ ATF_TC_HEAD(test_sqrt2, tc)
 }
 ATF_TC_BODY(test_sqrt2, tc)
 {
-	double x;
+	float fy;
+	double dy;
+	long double ldy;
+	size_t i, N;
 
-#ifdef	NAN
-	/* If x is NaN, a NaN shall be returned	*/
-	ATF_CHECK(fpclassify(sqrt(x)) == FP_NAN);
-#endif
+	N = sizeof(t2table) / sizeof(t2table[0]);
 
-	/* If x is +-0, x shall be returned */
-	x = +0.0;
-	ATF_CHECK(fpclassify(sqrt(x)) == FP_ZERO);
-	ATF_CHECK(signbit(sqrt(x)) == 0);
+	for (i = 0; i < N; i++) {
+		/* float */
+		fy = sqrtf(t2table[i].x);
+		ATF_CHECK(fpcmp_equal(fy, (float)t2table[i].y));
 
-	x = -0.0;
-	ATF_CHECK(fpclassify(sqrt(x)) == FP_ZERO);
-	ATF_CHECK(signbit(sqrt(x)) != 0);
+		/* double */
+		dy = sqrt(t2table[i].x);
+		ATF_CHECK(fpcmp_equal(dy, (double)t2table[i].y));
 
-#ifdef	INFINITY
-	/* If x is +Inf, x shall be returned */
-	x = INFINITY;
-	ATF_CHECK(fpclassify(sqrt(x)) == FP_INFINITE);
-	ATF_CHECK(signbit(sqrt(x)) == 0);
-#endif
+		/* long double */
+		ldy = sqrtl(t2table[i].x);
+		ATF_CHECK(fpcmp_equal(ldy, t2table[i].y));
+	}
 }
 
 /* Add test cases to test program */
