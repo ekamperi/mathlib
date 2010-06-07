a1ee23eb9cefe39aa4daa44606d4f3fd470e0323
stathis <stathis@opensolaris.(none)>
2010-06-07 16:09:16 +0300

    t_feraiseexcept: Abuse excepts argument

--- fenv/t_feraiseexcept.c
+++ fenv/t_feraiseexcept.c
@@ -83,11 +83,34 @@ ATF_TC_BODY(test_feraiseexcept2, tc)
 	ATF_CHECK(feraiseexcept(0) == 0);
 }
 
+/*
+ * Test case 3 -- Abuse `excepts' argument
+ */
+ATF_TC(test_feraiseexcept3);
+ATF_TC_HEAD(test_feraiseexcept3, tc)
+{
+  atf_tc_set_md_var(tc,
+		    "descr",
+		    "Abuse `excepts' argument");
+}
+ATF_TC_BODY(test_feraiseexcept3, tc)
+{
+	/* Cleas all exceptions */
+        ATF_CHECK(feclearexcept(FE_ALL_EXCEPT) == 0);
+
+	if (feraiseexcept(INT_MAX) == 0) {
+		ATF_CHECK(fetestexcept(FE_ALL_EXCEPT) == FE_ALL_EXCEPT);
+	} else {
+		ATF_CHECK(fetestexcept(FE_ALL_EXCEPT) == 0);
+	}
+}
+
 /* Add test cases to test program */
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, test_feraiseexcept);
 	ATF_TP_ADD_TC(tp, test_feraiseexcept2);
+	ATF_TP_ADD_TC(tp, test_feraiseexcept3);
 
 	return atf_no_error();
 }
