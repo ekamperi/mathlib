3dea347363003a6bcb3cd5400c50969aff6730e2
stathis <stathis@opensolaris.(none)>
2010-06-07 15:34:16 +0300

    t_fegetexceptflag: Abuse excepts argument

--- fenv/t_fegetexceptflag.c
+++ fenv/t_fegetexceptflag.c
@@ -1,5 +1,6 @@
 #include <atf-c.h>
 #include <fenv.h>
+#include <limits.h>	/* For INT_MAX */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,21 +24,24 @@ int extable[] = {
 	FE_OVERFLOW,
 #endif
 #ifdef	FE_UNDERFLOW
-	FE_UNDERFLOW
+	FE_UNDERFLOW,
+#endif
+#ifdef	FE_ALL_EXCEPT
+	FE_ALL_EXCEPT
 #endif
 };
 
 /*
- * fegetexceptflag()
+ * Test case 1 -- Basic functionality
  */
-ATF_TC(test_fegetexceptflag);
-ATF_TC_HEAD(test_fegetexceptflag, tc)
+ATF_TC(test_fegetexceptflag1);
+ATF_TC_HEAD(test_fegetexceptflag1, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
 	    "XXX");
 }
-ATF_TC_BODY(test_fegetexceptflag, tc)
+ATF_TC_BODY(test_fegetexceptflag1, tc)
 {
 	fexcept_t flag;
 	int comb, mth, nelms;
@@ -48,10 +52,34 @@ ATF_TC_BODY(test_fegetexceptflag, tc)
 	}
 }
 
+/*
+ * Test case 2 -- Abuse `excepts' argument
+ */
+ATF_TC(test_fegetexceptflag2);
+ATF_TC_HEAD(test_fegetexceptflag2, tc)
+{
+  atf_tc_set_md_var(tc,
+		    "descr",
+		    "Abuse `excepts' argument");
+}
+ATF_TC_BODY(test_fegetexceptflag2, tc)
+{
+	int ex1, ex2 = -12345;
+
+	ATF_REQUIRE(fegetexceptflag(&ex1, FE_ALL_EXCEPT) == 0);
+
+	if (fegetexceptflag(&ex2, INT_MAX) == 0) {
+		ATF_CHECK(ex1 == ex2);
+	} else {
+		ATF_CHECK(ex2 == -12345);
+	}
+}
+
 /* Add test cases to test program */
 ATF_TP_ADD_TCS(tp)
 {
-	ATF_TP_ADD_TC(tp, test_fegetexceptflag);
+	ATF_TP_ADD_TC(tp, test_fegetexceptflag1);
+	ATF_TP_ADD_TC(tp, test_fegetexceptflag2);
 
 	return atf_no_error();
 }
