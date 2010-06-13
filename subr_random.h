8aa6b0f8a6f1c75aea84b81cfef218fa70cb1372
stathis <stathis@opensolaris.(none)>
2010-06-13 17:47:57 +0300

    subr_random: Add missing prototype

--- subr_random.h
+++ subr_random.h
@@ -2,12 +2,14 @@
 #define	__SUBR_RANDOM_H__
 
 #include <inttypes.h>
+#include <math.h>
+
+void init_randgen(void);
 
 float random_float(int class);
 double random_double(int class);
 long double random_long_double(int class);
 
-int
-isvalidfp(const uint32_t *y);
+int isvalidfp(const uint32_t *y);
 
 #endif	/* ! __SUBR_RANDOM_H__ */
