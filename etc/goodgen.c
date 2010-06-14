f40ce656f0ee47113ef901403ac39baa2ebe0a2b
stathis <stathis@opensolaris.(none)>
2010-06-14 22:46:17 +0300

    goodgen: Add support for long double
    Not ready yet. We need to add support for *l() in ulps/gen.c

--- etc/goodgen.c
+++ etc/goodgen.c
@@ -30,7 +30,8 @@ main(int argc, char *argv[])
 	const char *type;
         int  min, max, total;
         int opt;
-        double dlower, dupper;
+	double dlower, dupper;
+	long double ldlower, ldupper;
 
 	/*
 	 * Save program name, function name and floating
@@ -97,9 +98,19 @@ main(int argc, char *argv[])
 		gen_double(fname, total, dlower, dupper);
         }
 
+	if (!strcmp(type, "ldouble")) {
+		if (min == 0 && max == 0) {
+			ldlower = -LDBL_MAX;
+			ldupper =  LDBL_MAX;
+		} else {
+			ldlower = min;
+			ldlower = max;
+		}
+		gen_ldouble(fname, total, ldlower, ldupper);
+	}
+
 	return (EXIT_SUCCESS);
 }
-
 
 static void
 gen_double(const char *fname, size_t total, double lower, double upper)
@@ -139,6 +150,44 @@ gen_double(const char *fname, size_t total, double lower, double upper)
 	mpfr_clear(mp_exact);
 }
 
+static void
+gen_ldouble(const char *fname, size_t total,
+    long double lower, long double upper)
+{
+	const struct fentry *f;
+        mpfr_t mp_x, mp_exact;
+        long double x, exact;
+        size_t i;
+
+        assert(fname);
+        f = getdomfunc(fname);
+        assert(f);
+        assert(f->f_narg == 1 || f->f_narg == 2);
+
+        /* Initialize high precision variables */
+        mpfr_init2(mp_x,     500);
+        mpfr_init2(mp_exact, 500);
+
+        for (i = 0; i < total; i++) {
+                do {
+                        x = random_long_double(FP_NORMAL);
+                } while (x < lower || x > upper);
+
+                /* Set the mpfr variables */
+                mpfr_set_ld(mp_x,       x, GMP_RNDN);
+                mpfr_set_ld(mp_exact, 0.0, GMP_RNDN);
+
+                f->f_mpfr(mp_exact, mp_x, GMP_RNDN);
+
+                exact = mpfr_get_ld(mp_exact, GMP_RNDN);
+
+                printf("x = % .35Le\t%s = % .35Le\n", x, fname, exact);
+        }
+
+        /* Free resources */
+        mpfr_clear(mp_x);
+        mpfr_clear(mp_exact);
+}
 
 static void
 usage(const char *progname)
