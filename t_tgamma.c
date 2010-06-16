e8e97f56c75b43a9e3fabf0690054ba60e4281bc
stathis <stathis@opensolaris.(none)>
2010-06-16 17:06:17 +0300

    t_tgamma: Add dozens of checks.  Yay.

--- t_tgamma.c
+++ t_tgamma.c
@@ -106,44 +106,234 @@ ATF_TC_BODY(test_tgamma2, tc)
 }
 
 /*
- * Test case 3 -- Deliberate overflow
+ * Test case 3 -- Edge cases
  */
 ATF_TC(test_tgamma3);
 ATF_TC_HEAD(test_tgamma3, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "For IEEE Std 754-1985 double, overflow happens when "
-	    "0 < x < 1/DBL_MAX, and 171.7 < x");
+	    "Check some edge cases");
 }
 ATF_TC_BODY(test_tgamma3, tc)
 {
+	/* If x is NaN, a NaN shall be returned */
+#ifdef	NAN
+	ATF_CHECK(isnan(tgamma(NAN)));
+	ATF_CHECK(isnan(tgammaf(NAN)));
+	ATF_CHECK(isnan(tgammal(NAN)));
+#endif
+
+	/* If x is +Inf, x shall be returned */
+#ifdef	INFINITY
+	ATF_CHECK(isinf(tgamma(INFINITY)));
+	ATF_CHECK(isinf(tgammaf(INFINITY)));
+	ATF_CHECK(isinf(tgammal(INFINITY)));
+#endif
+#ifdef	HUGE_VAL
+	ATF_CHECK(isinf(tgamma(HUGE_VAL)));
+	ATF_CHECK(isinf(tgammaf(HUGE_VAL)));
+	ATF_CHECK(isinf(tgammal(HUGE_VAL)));
+#endif
+#ifdef	HUGE_VALF
+	ATF_CHECK(isinf(tgamma(HUGE_VALF)));
+	ATF_CHECK(isinf(tgammaf(HUGE_VAL)));
+	ATF_CHECK(isinf(tgammal(HUGE_VALL)));
+#endif
+#ifdef	HUGE_VALL
+	ATF_CHECK(isinf(tgamma(HUGE_VALL)));
+	ATF_CHECK(isinf(tgammaf(HUGE_VALF)));
+	ATF_CHECK(isinf(tgammal(HUGE_VALL)));
+#endif
+}
+
+
+/*
+ * Test case 4 -- Pole error
+ */
+long t4table[] = { +0.0, -0.0 };
+
+ATF_TC(test_tgamma4);
+ATF_TC_HEAD(test_tgamma4, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "If x is +-0, a pole error shall occur");
+}
+ATF_TC_BODY(test_tgamma4, tc)
+{
+	int haserrexcept;
+	int haserrno;
+	int i;
+	float fy;
+	double dy;
+	long double ldy;
+
+	for (i = 0; i < 2; i++ ) {
+		/* float */
+		errno = 0;
+		clear_exceptions();
+		fy = tgammaf((float)t4table[i]);
+#ifdef  HUGE_VALF
+		ATF_CHECK(fy == HUGE_VALF);
+#endif
+		ATF_CHECK(iserrno_equalto(ERANGE));
+		ATF_CHECK(raised_exceptions(FE_DIVBYZERO));
+
+		/* double */
+		errno = 0;
+		clear_exceptions();
+		dy = tgamma((double)t4table[i]);
+#ifdef	HUGE_VAL
+		ATF_CHECK(dy == HUGE_VAL);
+#endif
+		ATF_CHECK(iserrno_equalto(ERANGE));
+		ATF_CHECK(raised_exceptions(FE_DIVBYZERO));
+
+		/* long double */
+		errno = 0;
+		clear_exceptions();
+		ldy = tgammal(t4table[i]);
+#ifdef  HUGE_VALL
+		ATF_CHECK(ldy == HUGE_VALL);
+#endif
+		ATF_CHECK(iserrno_equalto(ERANGE));
+		ATF_CHECK(raised_exceptions(FE_DIVBYZERO));
+	}
+
+	/*
+	 * Revenge is a Dish Best Served Cold :)
+	 *
+	 * We put this check here and not in the beginning of the test case,
+	 * because we don't want the lack of error handling support to
+	 * block the check against HUGE_VAL*. Nor do we want to split the test
+	 * case into 2.
+	 */
+	query_errhandling(&haserrexcept, &haserrno);
+	ATF_REQUIRE(haserrexcept || haserrno);
+}
+
+/*
+ * Test case 5 -- Domain error
+ */
+long double t5table[] = {
+#ifdef	INFINITY
+	-INFINITY,
+#endif
+#ifdef	HUGE_VAL
+	-HUGE_VAL,
+#endif
+#ifdef	HUGE_VALF
+	-HUGE_VALF,
+#endif
+#ifdef	HUGE_VALL
+	-HUGE_VALL
+#endif
+};
+
+ATF_TC(test_tgamma5);
+ATF_TC_HEAD(test_tgamma5, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "If x is -Inf, a domain error shall occur");
+}
+ATF_TC_BODY(test_tgamma5, tc)
+{
+	int haserrexcept;
+	int haserrno;
+	int i, N;
+	float fy;
+	double dy;
+	long double ldy;
+
+	N = sizeof(t5table) / sizeof(t5table[0]);
+	ATF_REQUIRE(N > 0);
+
+	for (i = 0; i < N; i++) {
+		/* float */
+		errno = 0;
+		clear_exceptions();
+		fy = tgammaf((float)t5table[i]);
+#ifdef	NAN
+		ATF_CHECK(isnan(fy));
+#endif
+		ATF_CHECK(iserrno_equalto(EDOM));
+		ATF_CHECK(raised_exceptions(MY_FE_INVALID));
+
+		/* double */
+		errno = 0;
+		clear_exceptions();
+		dy = tgamma((double)t5table[i]);
+#ifdef	NAN
+		ATF_CHECK(isnan(dy));
+#endif
+		ATF_CHECK(iserrno_equalto(EDOM));
+		ATF_CHECK(raised_exceptions(MY_FE_INVALID));
+
+		/* long double */
+		errno = 0;
+		clear_exceptions();
+		ldy = tgammal(t5table[i]);
+#ifdef  NAN
+		ATF_CHECK(isnan(ldy));
+#endif
+		ATF_CHECK(iserrno_equalto(EDOM));
+		ATF_CHECK(raised_exceptions(MY_FE_INVALID));
+	}
+
+	/*
+	 * Revenge is a Dish Best Served Cold :)
+	 *
+	 * We put this check here and not in the beginning of the test case,
+	 * because we don't want the lack of error handling support to
+	 * block the check against NANs. Nor do we want to split the test
+	 * case into 2.
+	 */
+	query_errhandling(&haserrexcept, &haserrno);
+	ATF_REQUIRE(haserrexcept || haserrno);
+}
+
+/*
+ * Test case 6 -- Deliberate overflow
+ */
+ATF_TC(test_tgamma6);
+ATF_TC_HEAD(test_tgamma6, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "For IEEE Std 754-1985 double, overflow happens when "
+	    "0 < x < 1/DBL_MAX, and 171.7 < x");
+}
+ATF_TC_BODY(test_tgamma6, tc)
+{
 	double x;
-	size_t i;
+	long i, N;
+	int haserrexcept;
+	int haserrno;
 
 	/*
 	 * We can't proceed if there's no way to detect errors,
 	 * especially overflows.
 	 */
-	int haserrexcept;
-	int haserrno;
-
 	query_errhandling(&haserrexcept, &haserrno);
 	ATF_REQUIRE(haserrexcept || haserrno);
 
-	/* Initialize error handling */
-	clear_exceptions();
-	clear_errno();
+	N = get_config_var_as_long(tc, "iterations");
+	ATF_REQUIRE(N > 0);
 
-	for (i = 0; i < 1000000; i++) {
+	ATF_FOR_LOOP(i, N, i++) {
 		do {
-			x = drand48();
-		} while (x == 0.0);
+			x = random_double(FP_NORMAL);
+		} while (x <= 171.7 && (x <= 0 || x >= 1/DBL_MAX));
 
-		x += 171.7;
+		errno = 0;
+		clear_exceptions();
+		(void)tgamma(x);
+		ATF_PASS_OR_BREAK(iserrno_equalto(ERANGE));
+		ATF_PASS_OR_BREAK(raised_exceptions(FE_OVERFLOW));
 	}
 }
-
 
 /* Add test cases to test program */
 ATF_TP_ADD_TCS(tp)
@@ -151,6 +341,9 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, test_tgamma1);
 	ATF_TP_ADD_TC(tp, test_tgamma2);
 	ATF_TP_ADD_TC(tp, test_tgamma3);
+	ATF_TP_ADD_TC(tp, test_tgamma4);
+	ATF_TP_ADD_TC(tp, test_tgamma5);
+	ATF_TP_ADD_TC(tp, test_tgamma6);
 
 	return atf_no_error();
 }
