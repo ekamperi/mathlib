db259a800f887810f0781144d5d9e4234d0cffca
Stathis Kamperis <ekamperi@gmail.com>
2010-06-27 02:47:38 +0300

    t_fmax: Cleanup

--- t_fmax.c
+++ t_fmax.c
@@ -1,8 +1,5 @@
 #include <atf-c.h>
 #include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 #include "subr_atf.h"
 #include "subr_fpcmp.h"
@@ -11,7 +8,8 @@
 /*
  * Test case 1 -- Basic functionality
  */
-struct t1entry {
+static const struct
+t1entry {
 	double x;       /* Input */
 	double y;	/* Input */
 	int z;		/* fmax output (0 for x>y, 1 otherwise */
@@ -68,10 +66,11 @@ ATF_TC_BODY(test_fmax1, tc)
 /*
  * Test case 2 -- Edge cases
  */
-struct t2entry {
-	double x;       /* Input */
-	double y;       /* Input */
-	double z;	/* fmax() output */
+static const struct
+t2entry {
+	long double x;	/* Input */
+	long double y;	/* Input */
+	long double z;	/* fmax() output */
 } t2table[] = {
 #ifdef	NAN
 	/*
@@ -91,6 +90,27 @@ struct t2entry {
 	{ +INFINITY, NAN, +INFINITY },
 	{ -INFINITY, NAN, -INFINITY },
 #endif	/* INFINITY */
+#ifdef	HUGE_VAL
+	{ NAN, +HUGE_VAL, +HUGE_VAL },
+	{ NAN, -HUGE_VAL, -HUGE_VAL },
+
+	{ +HUGE_VAL, NAN, +HUGE_VAL },
+	{ -HUGE_VAL, NAN, -HUGE_VAL },
+#endif  /* HUGE_VAL */
+#ifdef  HUGE_VALF
+	{ NAN, +HUGE_VALF, +HUGE_VALF },
+	{ NAN, -HUGE_VALF, -HUGE_VALF },
+
+	{ +HUGE_VALF, NAN, +HUGE_VALF },
+	{ -HUGE_VALF, NAN, -HUGE_VALF },
+#endif  /* HUGE_VALF */
+#ifdef  HUGE_VALL
+	{ NAN, +HUGE_VALL, +HUGE_VALL },
+	{ NAN, -HUGE_VALL, -HUGE_VALL },
+
+	{ +HUGE_VALL, NAN, +HUGE_VALL },
+	{ -HUGE_VALL, NAN, -HUGE_VALL },
+#endif  /* HUGE_VALL */
 
 	/* If x and y are NaN, a NaN shall be returned */
 	{ NAN, NAN, NAN }
@@ -107,14 +127,30 @@ ATF_TC_HEAD(test_fmax2, tc)
 ATF_TC_BODY(test_fmax2, tc)
 {
 	size_t i, N;
-	double oval;	/* fmax() output value */
 
 	N = sizeof(t2table) / sizeof(t2table[0]);
 	ATF_REQUIRE(N > 0);
 
 	for (i = 0; i < N; i++) {
-		oval = fmax(t2table[i].x, t2table[i].y);
-		ATF_CHECK(fpcmp_equal(oval, t2table[i].z));
+		/* float */
+		ATF_CHECK(fpcmp_equalf(
+			    fmaxf((float)t2table[i].x,
+				  (float)t2table[i].y),
+			    (float)t2table[i].z));
+
+		/* double */
+		ATF_CHECK(fpcmp_equal(
+			    fmax((double)t2table[i].x,
+				 (double)t2table[i].y),
+			    (double)t2table[i].z));
+
+		/* long double */
+#ifdef	HAVE_FMAXL
+		ATF_CHECK(fpcmp_equall(
+			    fmaxl(t2table[i].x,
+				  t2table[i].y),
+			    t2table[i].z));
+#endif
 	}
 }
 
