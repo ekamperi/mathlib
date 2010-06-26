a551a2bf13b6b425e6df723126bd2038498fd76e
stathis <stathis@opensolaris.(none)>
2010-06-26 21:11:13 +0300

    include/subr_atf: Introduce ATF_CHECK_IFNAN(), plus comments

--- include/subr_atf.h
+++ include/subr_atf.h
@@ -2,6 +2,7 @@
 #define	__SUBR_ATF_H__
 
 #include <atf-c.h>
+#include <math.h>
 
 bool get_config_var_as_bool(const atf_tc_t *tc, const char *name);
 long get_config_var_as_long(const atf_tc_t *tc, const char *name);
@@ -11,7 +12,15 @@ do {									\
 	atf_tc_fail_check(__FILE__, __LINE__, fmt, ##__VA_ARGS__);	\
 } while(0)
 
-/* Hacky, ugly, fragile. But it gets the job done */
+/*
+ * We always check the value of `__cond' before updating it, because
+ * one good deed cannot make a bad act even. E.g.,
+ *
+ * ATF_FOR_LOOP(i, N, i++) {
+ *	ATF_PASS_OR_BREAK(... false ...);
+ *	ATF_PASS_OR_BREAK(... true  ...);
+ * }
+ */
 #define ATF_PASS_OR_BREAK(x)			\
 do {						\
 	__cond = __cond ? (x) : 0;		\
@@ -19,8 +28,19 @@ do {						\
 		ATF_PRINT_ERRMSG(#x);		\
 } while(0)
 
+/* We can have only one such loop within a function */
 #define ATF_FOR_LOOP(i, end, incr)		\
 int __cond = 1;					\
 for (i = 0; i < (end) && __cond; incr)
+
+/*
+ * If host doesn't support NaNs, quietly return 1 as if NaNs were supported
+ * and the argument was indeed NaN. This effectively disables the check.
+ */
+#ifdef	NAN
+#define ATF_CHECK_IFNAN(x) ATF_CHECK(isnan(x))
+#else
+#define	ATF_CHECK_IFNAN(x) 1
+#endif
 
 #endif	/* ! __SUBR_ATF_H__ */
