4475b1198870de247362ac1cdcd30b2b1db42273
Stathis Kamperis <ekamperi@gmail.com>
2010-07-04 02:12:18 +0300

    Initial import of t_self

--- Makefile
+++ Makefile
@@ -12,7 +12,8 @@ all:    t_acos t_acosh t_asin t_asinh t_atan t_atan2 t_atanh t_cbrt t_ceil \
 	t_constants t_copysign t_cos t_cosh t_erf t_erfc t_exp t_exp2 t_expm1 t_fabs t_fdim \
 	t_float t_floor t_fmax t_fpclassify t_fpmacros t_ilogb t_lrint t_mac t_tgamma \
 	t_hypot t_log t_log10 t_log1p t_log2 t_logb t_ldexp t_nextafter t_pow \
-	t_rint t_signbit t_sin t_sinh t_sqrt t_trig_ident t_tan t_tanh t_trunc t_types
+	t_rint t_self t_signbit t_sin t_sinh t_sqrt t_trig_ident t_tan t_tanh \
+	t_trunc t_types
 
 obj:
 	@mkdir -p obj
@@ -261,6 +262,12 @@ _DEPS_T_RINT = t_rint.o subr_atf.o subr_errhandling.o subr_fpcmp.o subr_random.o
  DEPS_T_RINT = $(_DEPS_T_RINT:%=obj/%)
 t_rint: $(DEPS_T_RINT)
 	$(CC99) -o t_rint $(DEPS_T_RINT) $(CFLAGS) $(LIBS)
+
+
+_DEPS_T_SELF = t_self.o subr_atf.o subr_random.o
+ DEPS_T_SELF = $(_DEPS_T_SELF:%=obj/%)
+t_self: $(DEPS_T_SELF)
+	$(CC99) -o t_self $(DEPS_T_SELF) $(CFLAGS) $(LIBS)
 
 
 _DEPS_T_SIGNBIT = t_signbit.o subr_atf.o subr_random.o
---
+++ t_self.c
@@ -0,0 +1,59 @@
+#include <atf-c.h>
+#include <math.h>
+#include <stdio.h>
+
+#include "subr_atf.h"
+#include "subr_random.h"
+
+ATF_TC(test_rand1);
+ATF_TC_HEAD(test_rand1, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "Check that our random generator outpus valid fp numbers");
+}
+ATF_TC_BODY(test_rand1, tc)
+{
+	float fx, fy;
+	double dx, dy;
+	long double ldx, ldy;
+	long i, N;
+
+	N = get_config_var_as_long(tc, "iterations");
+	ATF_REQUIRE(N > 0);
+
+	/*
+	 * This check may look like totally pointless, but it's not.
+	 * It really catches invalid floating point numbers.
+	 */
+	ATF_FOR_LOOP(i, N, i++) {
+		/* float */
+		fx = random_float(FP_NORMAL);
+		fy = random_float(FP_NORMAL);
+		ATF_PASS_OR_BREAK(fx < fy || fx >= fy);
+		ATF_PASS_OR_BREAK(fpclassify(1.0 * fx) == FP_NORMAL);
+		ATF_PASS_OR_BREAK(fpclassify(1.0 * fy) == FP_NORMAL);
+
+                /* double */
+                dx = random_double(FP_NORMAL);
+                dy = random_double(FP_NORMAL);
+                ATF_PASS_OR_BREAK(dx < dy || dx >= dy);
+                ATF_PASS_OR_BREAK(fpclassify(1.0 * dx) == FP_NORMAL);
+                ATF_PASS_OR_BREAK(fpclassify(1.0 * dy) == FP_NORMAL);
+
+                /* long double */
+                ldx = random_long_double(FP_NORMAL);
+                ldy = random_long_double(FP_NORMAL);
+                ATF_PASS_OR_BREAK(ldx < ldy || ldx >= ldy);
+                ATF_PASS_OR_BREAK(fpclassify(1.0 * ldx) == FP_NORMAL);
+                ATF_PASS_OR_BREAK(fpclassify(1.0 * ldy) == FP_NORMAL);
+	}
+}
+
+/* Add test cases to test program */
+ATF_TP_ADD_TCS(tp)
+{
+	ATF_TP_ADD_TC(tp, test_rand);
+
+	return atf_no_error();
+}
