73ad20d6649730f08a81df25fa5552a95ec7c1c4
Stathis Kamperis <ekamperi@gmail.com>
2010-07-06 01:11:40 +0300

    Initial import of testrand2

---
+++ etc/testrand2.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "subr_random.h"
+
+struct
+stats {
+	const char *st_type;
+	size_t st_normals;
+	size_t st_denormals;
+	size_t st_nans;
+	size_t st_inf;
+	size_t st_zero;
+	size_t st_invalid;
+};
+
+#define DECL(type)						\
+static void							\
+probe_##type(struct stats *st, size_t N)			\
+{								\
+	type x;							\
+	size_t i;						\
+								\
+	assert(st);						\
+	memset(st, 0, sizeof(*st));				\
+								\
+	st->st_type = #type;					\
+								\
+	for (i = 0; i < N; i++) {				\
+		x = random_##type(0);				\
+								\
+		switch (fpclassify(x)) {			\
+		case FP_NORMAL:					\
+			st->st_normals++;			\
+			break;					\
+		case FP_SUBNORMAL:				\
+			st->st_denormals++;			\
+			break;					\
+		case FP_NAN:					\
+			st->st_nans++;				\
+			break;					\
+		case FP_INFINITE:				\
+			st->st_inf++;				\
+			break;					\
+		case FP_ZERO:					\
+			st->st_zero++;				\
+			break;					\
+		default:					\
+			/* invalid floating-point number */	\
+			st->st_invalid++;			\
+			break;					\
+		}						\
+	}							\
+}
+
+typedef long double long_double;
+DECL(float);
+DECL(double);
+DECL(long_double);
+
+static void
+print_stats(const struct stats *st)
+{
+	assert(st);
+
+	printf("---------- %s ----------\n", st->st_type);
+	printf("  Normals: %u\n", st->st_normals);
+        printf("Denormals: %u\n", st->st_denormals);
+        printf("     NaNs: %u\n", st->st_nans);
+        printf("      Inf: %u\n", st->st_inf);
+        printf("     Zero: %u\n", st->st_zero);
+	printf("  Invalid: %u\n", st->st_invalid);
+	fflush(stdout);
+}
+
+int
+main(void)
+{
+	struct stats s;
+	const size_t N = 100000;
+
+	/* floats */
+	probe_float(&s, N);
+	print_stats(&s);
+#if 0
+	/* double */
+        probe_double(&s, N);
+        print_stats(&s);
+
+	/* long double */
+        probe_long_double(&s, N);
+        print_stats(&s);
+#endif
+	return 0;
+}
