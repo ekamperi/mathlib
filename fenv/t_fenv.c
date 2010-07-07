f0f212d1a03a84b6ea7bf17683bfa5ca2ce73c14
stathis <stathis@opensolaris.(none)>
2010-07-07 12:42:07 +0300

    fenv/t_fenv: Cleanup a bit

--- fenv/t_fenv.c
+++ fenv/t_fenv.c
@@ -5,57 +5,97 @@
 #include <atf-c.h>
 
 /*
- * Test case 1 -- 
+ * Test case 1
  */
 ATF_TC(test_fenv1);
 ATF_TC_HEAD(test_fenv1, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "FE_ALL_EXCEPT is the bitwise-inclusive OR of all floating-point "
-	    "exception constants");
+	    "FE_ALL_EXCEPT is the bitwise-inclusive OR "
+	    "of all floating-point exception constants");
 }
 ATF_TC_BODY(test_fenv1, tc)
 {
+	/*
+	 * The <fenv.h> header shall define the following constants
+	 * _if and only if_ the implementation supports getting and setting.
+	 * So, protect them with #ifdefs.
+	 */
+#ifdef	FE_DIVBYZERO
 	ATF_CHECK(FE_ALL_EXCEPT & FE_DIVBYZERO);
-        ATF_CHECK(FE_ALL_EXCEPT & FE_INEXACT);
-        ATF_CHECK(FE_ALL_EXCEPT & FE_INVALID);
-        ATF_CHECK(FE_ALL_EXCEPT & FE_OVERFLOW);
-        ATF_CHECK(FE_ALL_EXCEPT & FE_UNDERFLOW);
+#endif
+#ifdef	FE_INEXACT
+	ATF_CHECK(FE_ALL_EXCEPT & FE_INEXACT);
+#endif
+#ifdef	FE_INVALID
+	ATF_CHECK(FE_ALL_EXCEPT & FE_INVALID);
+#endif
+#ifdef	FE_OVERFLOW
+	ATF_CHECK(FE_ALL_EXCEPT & FE_OVERFLOW);
+#endif
+#ifdef	FE_UNDERFLOW
+	ATF_CHECK(FE_ALL_EXCEPT & FE_UNDERFLOW);
+#endif
 
-	/* XXX: Should we allow for more FE_* symbols ?  */
-	ATF_CHECK((FE_ALL_EXCEPT
-		& ~(FE_DIVBYZERO | FE_INEXACT | FE_INVALID | FE_OVERFLOW \
-		    | FE_UNDERFLOW)) == 0);
+	/*
+	 * Note: We should allow for more FE_* symbols.
+	 * For example, in x86 the FE_DENORMAL exception is supported.
+	 */
 }
 
 /*
- * Test case 2 -- 
+ * Test case 2 --
  */
 ATF_TC(test_fenv2);
 ATF_TC_HEAD(test_fenv2, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "Rounding direction integer constants shall be non-negative and distinct");
+	    "Rounding direction integer constants shall "
+	    "be non-negative and distinct");
 }
 ATF_TC_BODY(test_fenv2, tc)
 {
 	/* These values are non-negative */
-        ATF_CHECK(FE_DOWNWARD >= 0);
-        ATF_CHECK(FE_TONEAREST >= 0);
-        ATF_CHECK(FE_TOWARDZERO >= 0);
-        ATF_CHECK(FE_UPWARD >= 0);
+#ifdef	FE_DOWNWARD
+	ATF_CHECK(FE_DOWNWARD >= 0);
+#endif
+#ifdef	FE_TONEAREST
+	ATF_CHECK(FE_TONEAREST >= 0);
+#endif
+#ifdef	FE_TOWARDZERO
+	ATF_CHECK(FE_TOWARDZERO >= 0);
+#endif
+#ifdef	FE_UPWARD
+	ATF_CHECK(FE_UPWARD >= 0);
+#endif
 
 	/* These values shall be distinct */
+#ifdef	FE_DOWNWARD
+#ifdef	FE_TONEAREST
 	ATF_CHECK(FE_DOWNWARD != FE_TONEAREST);
+#endif
+#ifdef	FE_TOWARDZERO
 	ATF_CHECK(FE_DOWNWARD != FE_TOWARDZERO);
+#endif
+#ifdef	FE_UPWARD
 	ATF_CHECK(FE_DOWNWARD != FE_UPWARD);
+#endif
+#endif	/* FE_DOWNWARD */
 
+#ifdef	FE_TONEAREST
+#ifdef	FE_TOWARDZERO
 	ATF_CHECK(FE_TONEAREST != FE_TOWARDZERO);
+#endif
+#ifdef	FE_UPWARD
 	ATF_CHECK(FE_TONEAREST != FE_UPWARD);
+#endif
+#endif	/* FE_TONEAREST */
 
+#if defined(FE_TOWARDZERO) && defined(FE_UPWARD)
 	ATF_CHECK(FE_TOWARDZERO != FE_UPWARD);
+#endif
 }
 
 /* Add test cases to test program */
