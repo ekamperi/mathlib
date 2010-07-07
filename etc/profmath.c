11518169a105a92ccd98564478ff39b9c285887e
Stathis Kamperis <ekamperi@gmail.com>
2010-07-07 04:11:15 +0300

    Initial import of profmath

---
+++ etc/profmath.c
@@ -0,0 +1,182 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "gen.h"
+#include "subr_random.h"
+
+#define FILENAME(x)	#x ".txt"
+
+#define NSAMPLE (10*1000)
+
+/*
+ * They are esentially the same, but we still define them as if
+ * they were different, for conceptual consistency and clarity.
+ */
+#define MARK_START(tv1)						\
+	assert(gettimeofday(tv1, NULL) == 0);
+
+#define MARK_END(tv2)						\
+	assert(gettimeofday(tv2, NULL) == 0);
+
+#define USECS(tv1, tv2)						\
+	(((tv2).tv_sec  - (tv1).tv_sec ) * 1000000 +		\
+	 ((tv2).tv_usec - (tv1).tv_usec))
+
+/* Make the globals so that we allocate memory only once */
+double *dx = NULL;
+double *dy = NULL;
+double *dz = NULL;
+struct timeval *start = NULL;
+struct timeval *end   = NULL;
+
+static void
+init(void)
+{
+        dx    = malloc(NSAMPLE * sizeof(double));
+	dy    = malloc(NSAMPLE * sizeof(double));
+        dz    = malloc(NSAMPLE * sizeof(double));
+        start = malloc(NSAMPLE * sizeof(struct timeval));
+        end   = malloc(NSAMPLE * sizeof(struct timeval));
+
+        assert(dx && dy && dz && start && end);
+}
+
+static void
+reset(void)
+{
+	assert(dx && dy && dz && start && end);
+
+	memset(dx,    0, NSAMPLE * sizeof(double));
+        memset(dy,    0, NSAMPLE * sizeof(double));
+        memset(dz,    0, NSAMPLE * sizeof(double));
+        memset(start, 0, NSAMPLE * sizeof(double));
+        memset(end,   0, NSAMPLE * sizeof(double));
+}
+
+static void
+cleanup(void)
+{
+	assert(dx && dy && dz && start && end);
+
+	free(dx);
+	free(dy);
+	free(dz);
+	free(start);
+	free(end);
+
+	dx = dy = dz = NULL;
+	start = end = NULL;
+}
+
+static int
+proffunc(const char *fname)
+{
+	const struct fentry *f;
+	FILE *fp;
+	size_t i, j;
+
+	/* Lookup the function */
+	f = getfunctionbyname(fname);
+	if (f == NULL)
+		return (-1);
+
+	/* Zero out the arrays we will be writing at */
+	reset();
+
+	/* Generate random input -- do it before hand */
+	for (i = 0; i < NSAMPLE; i++) {
+		if (f->f_narg >= 1) {
+			do {
+				dx[i] = random_double(FP_NORMAL);
+			} while (!f->f_u.fp1(dx[i]));
+		}
+		if (f->f_narg >= 2) {
+			do {
+				dx[i] = random_double(FP_NORMAL);
+				dy[i] = random_double(FP_NORMAL);
+			} while (!f->f_u.fp2(dx[i], dy[i]));
+		}
+	}
+
+	/* Ready to go */
+	for (i = 0; i < NSAMPLE; i++) {
+		MARK_START(&start[i]);
+
+		for (j = 0; j < 100; j++) {
+			if (f->f_narg == 1) {
+				dz[i] = f->f_libm(dx[i]);
+			} else {
+				dz[i] = f->f_libm(dx[i], dy[i]);
+			}
+		}
+		
+		MARK_END(&end[i]);
+	}
+
+	/* Calculate diffs and dump them to the file */
+        fp = fopen(FILENAME(fname), "w");
+        assert(fp);
+
+	for (i = 0; i < NSAMPLE; i++) {
+		if (f->f_narg == 1)
+			fprintf(fp, "%.16e\t%.16e\t%ld\n",
+			    dx[i], dz[i], USECS(start[i], end[i]));
+		else
+			fprintf(fp, "%.16e\t%.16e\t%.16e\t%ld\n",
+                            dx[i], dy[i], dz[i], USECS(start[i], end[i]));
+	}
+
+	return (0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int i, rv, total, all;
+	const char *target;
+
+	/* Skip program name */
+	argv++;
+	argc--;
+
+	/*
+	 * If none argument or "all" is given, then we assume
+	 * that all functions should be probed.
+	 */
+	all = 0;
+	if (argc == 0 || (argc == 1 && !strcmp(argv[0], "all"))) {
+		argv++;
+		argc--;
+
+		all = 1;
+	}
+
+	/* Initialize random number generator */
+	init_randgen();
+
+	/* Allocate memory */
+	init();
+
+	total = all ? fsize : argc;
+
+	for (i = 0; i < total; i++) {
+		target = all ? ftable[i].f_name : argv[i];
+		rv = proffunc(target);
+		if (rv != -1) {
+		} else {
+			fprintf(stderr, "function: %s not probed -- skipping\n",
+			    argv[i]);
+			continue;
+		}
+	}
+
+	/* Free up resources */
+	cleanup();
+
+	return (EXIT_SUCCESS);
+}
