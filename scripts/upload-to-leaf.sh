a918feb130d321d17ed52e533e16ea52edfd58cb
stathis <stathis@opensolaris.(none)>
2010-07-10 14:59:33 +0300

    upload-to-leaf.sh: Add support for results or graphs alone

--- scripts/upload-to-leaf.sh
+++ scripts/upload-to-leaf.sh
@@ -12,10 +12,39 @@ HOSTNAME="leaf.dragonflybsd.org"
 REMOTELOGDIR="public_html/logs/$KERNEL/$RELEASE/$MACHINE"
 REMOTEGRAPHSDIR="public_html/graphs/$KERNEL/$RELEASE/$MACHINE"
 
-echo ">>> Uploading ATF results to $USERNAME@$HOSTNAME:~/$REMOTEDIR"
-scp results.html "$USERNAME@$HOSTNAME:~/$REMOTELOGDIR/$FILENAME"
-echo "DONE"
+usage()
+{
+    cat <<EOF
+Usage: `basename $0` all | graphs | results
+At least one of the above options must be specified.
+EOF
 
-echo ">>> Uploading graphs to $USERNAME@$HOSTNAME:~/$REMOTEDIR"
-scp ../etc/graphs/*.png "$USERNAME@$HOSTNAME:~/$REMOTEGRAPHSDIR/"
-echo "DONE"
+    exit 1
+}
+
+case "$1" in
+    "all")
+	all=1
+	;;
+    "graphs")
+	graphs=1
+	;;
+    "results")
+	results=1
+	;;
+    "")
+	usage
+	;;    
+esac
+
+if [ ! -z "$results" ] || [ -z "all" ]; then
+    echo ">>> Uploading ATF results to $USERNAME@$HOSTNAME:~/$REMOTEDIR"
+    scp results.html "$USERNAME@$HOSTNAME:~/$REMOTELOGDIR/$FILENAME"
+    echo "DONE"
+fi
+
+if [ ! -z "$graphs" ] || [ -z "all" ]; then
+    echo ">>> Uploading graphs to $USERNAME@$HOSTNAME:~/$REMOTEDIR"
+    scp ../etc/graphs/*.png "$USERNAME@$HOSTNAME:~/$REMOTEGRAPHSDIR/"
+    echo "DONE"
+fi
