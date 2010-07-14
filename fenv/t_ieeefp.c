bd9a443355bcb0cdd15a34156a4564c86c598a6a
stathis <stathis@opensolaris.(none)>
2010-07-14 16:41:31 +0300

    fenv/t_ieeefp.h: Cleanup

--- fenv/t_ieeefp.c
+++ fenv/t_ieeefp.c
@@ -1,30 +1,34 @@
+#include "config.h"
+
 #include <atf-c.h>
+#ifdef	HAVE_FENV_H
 #include <fenv.h>
-#include <ieeefp.h>	/* XXX: Use autoconf to check if available */
-#include <stdio.h>
-#include <string.h>
+#endif
+#ifdef	HAVE_IEEEFP_H
+#include <ieeefp.h>
+#endif
 
 #pragma STDC FENV_ACCESS ON
 
 /*
  * Test case 1
  */
-ATF_TC(test_fenv1);
-ATF_TC_HEAD(test_fenv1, tc)
+ATF_TC(test_ieeefp1);
+ATF_TC_HEAD(test_ieeefp1, tc)
 {
 	atf_tc_set_md_var(tc,
 	    "descr",
-	    "Check whether fenv.h (standard) and ieeepfp.h (non-standard)"
+	    "Check whether fenv.h (standard) and ieeefp.h (non-standard) "
 	    "can peacefully co-exist");
 }
-ATF_TC_BODY(test_fenv1, tc)
+ATF_TC_BODY(test_ieeefp1, tc)
 {
 }
 
 /* Add test cases to test program */
 ATF_TP_ADD_TCS(tp)
 {
-	ATF_TP_ADD_TC(tp, test_fenv1);
+	ATF_TP_ADD_TC(tp, test_ieeefp1);
 
 	return atf_no_error();
 }
