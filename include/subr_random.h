8aec934f9f90279048fd04b31fd3900d64488162
stathis <stathis@opensolaris.(none)>
2010-07-16 23:05:20 +0300

    include/subr_random.h: Add missing complex- headers

--- include/subr_random.h
+++ include/subr_random.h
@@ -2,12 +2,19 @@
 #define	__SUBR_RANDOM_H__
 
 #include <inttypes.h>
+#include <complex.h>
 
 void init_randgen(void);
 
+/* Real numbers */
 float random_float(int class);
 double random_double(int class);
 long double random_long_double(int class);
+
+/* Complex numbers */
+float complex random_float_complex(int class);
+double complex random_double_complex(int class);
+long double complex random_long_double_complex(int class);
 
 /* We export these functions, only to be used by self tests */
 int isvalidfp_vax(const uint32_t *y);
