ff03c9f9d43689c8665e5b468478df98b3d7eb03
stathis <stathis@opensolaris.(none)>
2010-07-18 16:34:13 +0300

    Initial import of fenv/t_overrun

---
+++ fenv/t_overrun.c
@@ -0,0 +1,51 @@
+#include <atf-c.h>
+#include <fenv.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "subr_atf.h"
+
+#pragma STDC FENV_ACCESS ON
+
+/*
+ * Test case 1
+ */
+ATF_TC(test_overrun1);
+ATF_TC_HEAD(test_overrun1, tc)
+{
+	atf_tc_set_md_var(tc,
+	    "descr",
+	    "Check for overrun");
+}
+ATF_TC_BODY(test_overrun1, tc)
+{
+	fenv_t env[2];
+	unsigned char bytes[sizeof(fenv_t)];
+	long N;
+	size_t i;
+
+	N = get_config_var_as_long(tc, "iterations");
+	ATF_REQUIRE(N > 0);
+
+	ATF_FOR_LOOP(i, N, i++) {
+		for (i = 0; i < sizeof(fenv_t); i++)
+			bytes[i] = rand() % 255;
+		memcpy(&env[1], bytes, sizeof(fenv_t));
+
+		ATF_REQUIRE(fegetenv(env) == 0);
+
+		/* One failure is enough to prove corruption */
+		for (i = 0; i < sizeof(fenv_t); i++)
+			ATF_PASS_OR_BREAK(
+				memcmp(&env[1], bytes, sizeof(fenv_t)) == 0);
+	}
+}
+
+/* Add test cases to test program */
+ATF_TP_ADD_TCS(tp)
+{
+	ATF_TP_ADD_TC(tp, test_overrun1);
+
+	return atf_no_error();
+}
