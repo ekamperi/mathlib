38b0f9f87b21542976bfd20efa6874a0aa754bba
stathis <stathis@opensolaris.(none)>
2010-07-25 17:23:53 +0300

    subr_fpcmp: Add untested support for fp complex comparisons

--- subr_fpcmp.c
+++ subr_fpcmp.c
@@ -1,6 +1,10 @@
+#include <complex.h>
 #include <math.h>
 #include "subr_fpcmp.h"
 
+/*******************************************************************************
+ *			Real arithmetic
+ ******************************************************************************/
 #define __FPCMP_COMMON(x, y)				\
 do {							\
 	if (isnan(x) || isnan(y))			\
@@ -37,4 +41,70 @@ fpcmp_equall(long double x, long double y)
 	__FPCMP_COMMON(x, y);
 
 	return ((fabsl(x - y) < (1E-10 * fmaxl(fabsl(x), fabsl(y)))));
+}
+
+/********************************************************************************
+ *				Complex arithmetic
+ ******************************************************************************/
+/*
+ * Quoting from ISO/IEC 9899:TC2:
+ *
+ * 6.2.5.13 Types
+ * Each complex type has the same representation and alignment requirements as
+ * an array type containing exactly two elements of the corresponding real type;
+ * the first element is equal to the real part, and the second element to the
+ * imaginary part, of the complex number.
+ */
+typedef union {
+	float complex z;
+	float parts[2];
+} float_complex;
+
+typedef union {
+	double complex z;
+	double parts[2];
+} double_complex;
+
+typedef union {
+	long double complex z;
+	long double parts[2];
+} long_double_complex;
+
+int
+fpcomplex_equalf(float complex x, float complex y)
+{
+	float_complex fcx = { .z = x };
+	float_complex fcy = { .z = y };
+	int real, imag;
+
+	real = fpcmp_equalf(fcx.parts[0], fcy.parts[0]);
+	imag = fpcmp_equalf(fcx.parts[1], fcy.parts[1]);
+
+	return (real && imag);
+}
+
+int
+fpcomplex_equal(double complex x, double complex y)
+{
+	double_complex dcx = { .z = x };
+	double_complex dcy = { .z = y };
+	int real, imag;
+
+	real = fpcmp_equal(dcx.parts[0], dcy.parts[0]);
+	imag = fpcmp_equal(dcx.parts[1], dcy.parts[1]);
+
+	return (real && imag);
+}
+
+int
+fpcomplex_equall(long double complex x, long double complex y)
+{
+	long_double_complex ldcx = { .z = x };
+	long_double_complex ldcy = { .z = y };
+	int real, imag;
+
+	real = fpcmp_equall(ldcx.parts[0], ldcy.parts[0]);
+	imag = fpcmp_equall(ldcx.parts[1], ldcy.parts[1]);
+
+	return (real && imag);
 }
