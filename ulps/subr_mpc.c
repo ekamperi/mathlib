3ea732829f501a37c44c8b5a7b8f750b89ef3b36
Stathis Kamperis <ekamperi@gmail.com>
2010-07-31 09:39:06 +0300

    subr_mpc: Implenet mpc_get_ld()

--- ulps/subr_mpc.c
+++ ulps/subr_mpc.c
@@ -27,7 +27,21 @@ mpc_get_dc(mpc_t op, mpc_rnd_t rnd)
 long double complex
 mpc_get_ldc(mpc_t op, mpc_rnd_t rnd)
 {
+	mpfr_t mp_real;
+	mpfr_t mp_imag;
 	long_double_complex z;
+
+	mpfr_init2(mp_real, 200);
+	mpfr_init2(mp_imag, 200);
+
+	mpc_real(mp_real, op, rnd);
+	mpc_imag(mp_imag, op, rnd);
+
+	z.parts[0] = mpfr_get_ld(mp_real, rnd);
+	z.parts[1] = mpfr_get_ld(mp_imag, rnd);
+
+	mpfr_clear(mp_real);
+	mpfr_clear(mp_imag);
 
 	return (z.z);
 }
