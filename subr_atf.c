f3b8ddfea0be81456ac81d19314ad5442dff3455
Stathis Kamperis <ekamperi@gmail.com>
2010-08-05 16:50:26 +0300

    subr_atf: Cosmetic

--- subr_atf.c
+++ subr_atf.c
@@ -24,8 +24,8 @@ print_undefined_var_msg(const char *name, const char *defval)
 
 	fprintf(stderr,
 	    "Variable '%s' appears to be undefined\n"
-	    "Please provide a value for it, either by running the test case\n"
-	    "via atf-run or by invoking it as <test> -v %s=<val>\n"
+	    "Please provide a value for it, either by running the test\n"
+	    "case via atf-run or by invoking it as <test> -v %s=<val>\n"
 	    "Using default value '%s'\n",
 	    name, name, defval);
 }
